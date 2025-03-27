505529c34aea3a8a462a22fd8cf99dae739ce22b
ayaanparashift <ayaan.s@parashifttech.com>
2025-03-27 14:02:17 +0530

    map hosur position changed

--- src/app/parks/chakan-i/componentsf/ParkInfo.jsx
+++ src/app/parks/chakan-i/componentsf/ParkInfo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 const ParkInfo = () => {
   const stats = [
-    { value: "0.85 Mln", label: "SQ. FT. of\nTotal Area" },
+    { value: "0.85 Mn", label: "SQ. FT. of\nTotal Area" },
     { value: "04 No.", label: "Total\nBuildings" },
     { value: "Pune,\nMaharashtra", label: "Park\nLocation" },
   ];

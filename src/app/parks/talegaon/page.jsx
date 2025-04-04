3644bb6cf5004639397ef2b3eacf8e3765b703c0
ayaanparashift <ayaan.s@parashifttech.com>
2025-04-04 16:34:32 +0530

    park-inside fix

--- src/app/parks/talegaon/page.jsx
+++ src/app/parks/talegaon/page.jsx
@@ -4,7 +4,7 @@ import TabMenu from "./componentsf/TabsMenu";
 import ParkOverview from "./componentsf/ParkOverview";
 import ParkInfo from "./componentsf/ParkInfo";
 import LastSw from "./componentsf/LastSw";
-import CharSlide from "./componentsf/CharSlide";
+// import CharSlide from "./componentsf/CharSlide";
 import PhVdslider from "./componentsf/PhVdslider";
 const page = () => {
   return (
